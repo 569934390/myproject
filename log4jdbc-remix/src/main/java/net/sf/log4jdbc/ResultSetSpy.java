2145b4b7edd14da69916165b0350182737417ce6
lifei <569934930@163.com>
2015-08-05 10:36:50 +0800

    文件修改

--- log4jdbc-remix/src/main/java/net/sf/log4jdbc/ResultSetSpy.java
+++ log4jdbc-remix/src/main/java/net/sf/log4jdbc/ResultSetSpy.java
@@ -127,7 +127,7 @@ public class ResultSetSpy implements ResultSet, Spy
     log = SpyLogFactory.getSpyLogDelegator();
     if (log.isResultSetCollectionEnabled())
     {
-      resultSetCollector = new DefaultResultSetCollector(log.isResultSetCollectionEnabledWithUnreadValueFillIn(),log.getMaxRow(),log.getMaxColumn());
+      resultSetCollector = new DefaultResultSetCollector(log);
     }
     reportReturn("new ResultSet", "", realResultSet);
   }
